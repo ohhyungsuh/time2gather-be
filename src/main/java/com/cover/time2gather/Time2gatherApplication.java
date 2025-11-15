eab73e9b2353bff27e0ed8e1092a1afb52e9dde2
baezzys <wlsdn3578@gmail.com>
2025-11-15 14:58:24 +0900

    initial commit

---
+++ src/main/java/com/cover/time2gather/Time2gatherApplication.java
@@ -0,0 +1,13 @@
+package com.cover.time2gather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Time2gatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Time2gatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cover/time2gather/Time2gatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cover.time2gather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Time2gatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
